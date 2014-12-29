4f0bf0409682577449b3b500f2900a65ff6f7989
rails <rails@rails>
2014-12-29 15:15:59 +0530

    initial commit

---
+++ config/deploy/production.rb
@@ -0,0 +1,6 @@
+set :domain, "23.227.172.79"
+set :rails_env, 'production'
+set :gitrepo, "git@github.com:neeraji2it/Rising.git"
+set :deploydir, "/var/www/rising.com/public_html"
+set :user, "hiperpool"
+set :use_sudo, false
