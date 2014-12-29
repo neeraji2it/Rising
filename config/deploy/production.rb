8407c70740ce61bb6535956756cee457193db605
rails <rails@rails>
2014-12-29 15:21:41 +0530

    added the configuration for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,5 +2,5 @@ set :domain, "23.227.172.79"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/Rising.git"
 set :deploydir, "/var/www/rising.com/public_html"
-set :user, "hiperpool"
+set :user, "shivraj"
 set :use_sudo, false
