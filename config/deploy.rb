ad8b689745e500bfac211906df67722600f64d69
rails <rails@rails.com>
2015-02-11 12:10:11 +0530

    - change deployment server to new heavy server - symlink payment-php code from shared directory - symlink token.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,8 @@ desc "Symlinks database.yml, mailer.yml file from shared directory into the late
 task :symlink_shared, :roles => [:app, :db] do
   run "ln -s #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
   run "ln -s #{shared_path}/config/secrets.yml #{latest_release}/config/secrets.yml"
+  run "ln -s #{shared_path}/payments #{latest_release}/public/payments"
+  run "ln -s #{shared_path}/payments/tokens.rb #{latest_release}/config/initializers/tokens.rb" 
 #  run "ln -s #{shared_path}/system #{latest_release}/system"
 #  run "ln -s #{shared_path}/public/system/attaches #{latest_release}/public/system/attaches"
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,16 @@
-set :domain, "23.227.172.79"
+# OLD SERVER CONFIGURATION
+
+# set :domain, "23.227.172.79"
+# set :rails_env, 'production'
+# set :gitrepo, "git@github.com:neeraji2it/Rising.git"
+# set :deploydir, "/var/www/rising.com/public_html"
+# set :user, "shivraj"
+# set :use_sudo, false
+
+
+set :domain, "23.92.65.127"
 set :rails_env, 'production'
 set :gitrepo, "git@github.com:neeraji2it/Rising.git"
 set :deploydir, "/var/www/rising.com/public_html"
-set :user, "shivraj"
+set :user, "neeraj"
 set :use_sudo, false
